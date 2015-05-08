0aa561b24af2fe900d50748d4cdffe6620f24286
Mark Ulrich <mulrich@stanford.edu>
2015-05-08 14:40:10 -0700

    starting features stuff

--- classifier/features.py
+++ classifier/features.py
@@ -4,18 +4,21 @@ import string
 from collections import Counter
 
 class Features:
-    MAX_CHARS_PER_WORD = 10
+    MAX_CHARS_PER_WORD = 20
 
     @staticmethod
     def len_feats(text):
-        MAX_CHARS_PER_WORD = 10
+        MAX_CHARS_PER_WORD = 20
         words = text.split()
         feats = {
             'n_chars': len(text),
             'n_words': len(words),
             'n_periods': sum(1 for c in text if c == '.'),
-            'n_questions': sum(1 for c in text if c == '?')
+            'n_questions': sum(1 for c in text if c == '?'),
+            'n_exclamations': sum(1 for c in text if c == '!')
         }
+        feats['avg_word_len'] = feats['n_chars'] / float(feats['n_words'])
+        feats['avg_sentance_len'] = feats['n_chars'] / float(feats['n_periods'] + feats['n_questions'] + feats['n_exclamations'])
         for i in range(1, MAX_CHARS_PER_WORD + 1):
             feats['n_' + str(i) + '_char_words'] = sum(1 for w in words if len(w) == i)
         return feats
