04c883c5f2bbc8e15ab94f915b060aa909c5aea5
Mark Ulrich <mulrich@stanford.edu>
2015-05-13 17:17:36 -0700

    Added bag of parts of speech, subjectivity/polarity feats

--- classifiers/features.py
+++ classifiers/features.py
@@ -1,4 +1,4 @@
-from itertools import groupby
+from collections import defaultdict
 
 def basic_feats(text):
         # Length features
@@ -13,7 +13,7 @@ def basic_feats(text):
             'n_upper': sum(1 for c in text if c.isupper())
         }
         feats['avg_word_len'] = feats['n_chars'] / float(feats['n_words'])
-        n_sentences = feats['n_periods'] + feats['n_questions'] + feats['n_exclamations'] + 1
+        n_sentences = max(feats['n_periods'] + feats['n_questions'] + feats['n_exclamations'], 1)
         feats['n_sentences'] = n_sentences
         feats['avg_sentence_len'] = feats['n_chars'] / float(n_sentences)
         feats['words_per_sentence'] = feats['n_words'] / float(n_sentences)
@@ -26,31 +26,78 @@ def basic_feats(text):
 
 def nltk_feats(text):
     import nltk
+    # Penn Discourse Treebank
     POS_TREEBANK_TAGS = {'CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS', 'PDT', 'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'SYM', 'TO', 'UH', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'WDT', 'WP', 'WP$', 'WRB'}
-    # NLTK features
+    MAX_CHARS_PER_WORD = 20
+
+    feats = defaultdict(float)
     tokens = nltk.word_tokenize(text)
+
+    # Basic length features.
+    feats['n_chars'] = len(text)
+    feats['n_words'] = len(tokens)
+    feats['n_periods'] = sum(1 for c in tokens if c == '.')
+    feats['n_questions'] = sum(1 for c in tokens if c == '?')
+    feats['n_exclamations'] = sum(1 for c in tokens if c == '!')
+    feats['n_upper'] = sum(1 for c in text if c.isupper())
+    feats['avg_word_len'] = feats['n_chars'] / float(feats['n_words'])
+    n_sentences = max(feats['n_periods'] + feats['n_questions'] + feats['n_exclamations'], 1)
+    feats['n_sentences'] = n_sentences
+    feats['avg_sentence_len'] = feats['n_chars'] / float(n_sentences)
+    feats['words_per_sentence'] = feats['n_words'] / float(n_sentences)
+    for i in range(1, MAX_CHARS_PER_WORD + 1):
+        # Record number/percent of tokens of length i.
+        key = str(i) + '_char_words'
+        n_iletter_words = sum(1 for w in tokens if len(w) == i)
+        feats['n_' + key] = n_iletter_words
+        feats['perc_' + key] = n_iletter_words / float(feats['n_words'])
+
+    # Extraneous feats
+    feats['starts_with_I'] = 1.0 if text[:2] == 'I ' else 0.0
+
+    # Bag of parts of speech
     tagged = nltk.pos_tag(tokens)
-    feats = {k: 0 for k in POS_TREEBANK_TAGS}
-    print tagged
-    for key, group in groupby(tagged, lambda x: x[0]):
-        feats[key] = sum(1 for _ in group)
+    feats['n_novel_tags'] = 0.0
+    for word, tag in tagged:
+        if tag not in POS_TREEBANK_TAGS:
+            feats['n_novel_tags'] += 1.0
+        feats[tag] += 1.0
     print feats
     return feats
 
 def textblob_feats(text):
     from textblob import TextBlob
+    feats = defaultdict(float)
+    blob = TextBlob(text)
+    feats['polarity'] = blob.polarity
+    feats['subjectivity'] = blob.subjectivity
+    return feats
 
-def similarity_word_feature(textA, textB):
-    splitA = textA.split()
-    splitB = textB.split()
-    num_same_words = len(set(splitA).intersection(set(splitB)))
-    num_total_words = len(set(splitA + splitB))
-    return float(num_same_words) / num_total_words
+def all_comment_feats(text):
+    f = nltk_feats(text)
+    f2 = textblob_feats(text)
+    assert len(set(f.keys()).union(f2.keys())) == 0
+    f.update(f2)
+    return f
+
+def jaccard_distance(textA, textB):
+    import nltk
+    from nltk.metrics.distance import jaccard_distance
+    return jaccard_distance(set(nltk.word_tokenize(textA)), set(nltk.word_tokenize(textB)))
 
 if __name__ == '__main__':
-    text = 'This is my. Comment?'
-    feats = basic_feats(text)
-    assert feats['n_chars'] == len(text)
-    assert feats['words_per_sentence'] == 2.0
-    assert feats['n_sentences'] == 2
+    simple = 'This is my comment'
+    feats = basic_feats(simple)
+    featsNLTK = nltk_feats(simple)
+    for k, v in feats.items():
+        assert feats[k] == featsNLTK[k]
+    assert feats['n_chars'] == len(simple)
+    print feats['perc_2_char_words']
+    assert feats['perc_2_char_words'] == 0.5
+    assert feats['n_sentences'] == 1
+    long = "I the only danger. But the only danger he had really been in was in the last half-hour of his imprisonment, when Owl, who had just flown up, sat on a branch of his tree to comfort him, and told him a very long story about an aunt who had once laid a seagull's egg by mistake, and the story went on and on, rather like this sentence."
+    feats = nltk_feats(long)
+    assert feats['starts_with_I'] == 1.0
+    assert feats['n_periods'] == 2
+    assert jaccard_distance('What how be', 'What how be 2') == 0.25
     print 'All tests passed'
